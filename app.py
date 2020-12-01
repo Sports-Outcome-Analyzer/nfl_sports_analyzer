2ea482ca1b200e8b86be621a842d5644532c2ca0
Tashi Tsering Gurung <tashi.gurung@baruchmail.cuny.edu>
2020-12-01 17:51:26 -0500

    flask w/ week filter

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 import flask
 import os
+import pickle
 import requests
 import pandas as pd
-from pandas import json_normalize 
+from pandas import json_normalize
 
 # get our api key
-apiKey = os.environ['sportsAPI']
-
+# apiKey = os.environ['sportAPI']
+apiKey = 'd8b5ea01537141eb9a320f95994b7109'
+default_week = 1
 # define flask app
 app = flask.Flask(__name__, template_folder='templates')
+
 
 # basic landing route
 @app.route('/', methods=['GET', 'POST'])
 def main():
     # make call for list of games
-    response = requests.get('https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/2020REG/10?key={0}'.format(apiKey))
+    response = requests.get(
+        'https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/2020REG/{0}?key=d8b5ea01537141eb9a320f95994b7109'.format(
+            default_week))
 
-
-    df = pd.DataFrame.from_dict(response.json())     # turn into dataframe
-    df = df[['AwayTeam','HomeTeam']] # transform into only team names
-
+    df = pd.DataFrame.from_dict(response.json())  # turn into dataframe
+    # df = df[['AwayTeam','HomeTeam']] # transform into only team names
 
     # load page
     if flask.request.method == 'GET':
-        return(flask.render_template('main_page.html', games=df))
+        return flask.render_template('main_page.html',
+                                     games=df)
+
+    if flask.request.method == 'POST':
+        selected_week = flask.request.form['week_user_selected']
+        response = requests.get(
+            'https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/2020REG/{0}?key=d8b5ea01537141eb9a320f95994b7109'.format(
+                selected_week))
+        df = pd.DataFrame.from_dict(response.json())
+        return flask.render_template('main_page.html',
+                                     games=df,
+                                     selected_week=selected_week)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
