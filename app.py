3a8405eb3345edc01841b0a233f32e1868b86b47
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-12-02 00:11:36 -0500

    updating flask app for each year to use database

--- app.py
+++ app.py
@@ -2,51 +2,150 @@ import flask
 import os
 import pickle
 import requests
+import psycopg2
 import pandas as pd
 from pandas import json_normalize
 
-# get our api key
-# apiKey = os.environ['sportAPI']
-apiKey = 'd8b5ea01537141eb9a320f95994b7109'
+
+# load in env variables
+db_host = os.environ['HEROKU_DB_HOST']
+db_name = os.environ['HEROKU_DB']
+db_user = os.environ['HEROKU_DB_USER']
+db_password = os.environ['HEROKU_DB_PASSWORD']
+api_key = os.environ['SPORTS_DATA_IO_API']
+
+# set default week
 default_week = 1
+
+# connect to postgres database
+conn = psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port="5432")
+cur = conn.cursor()
+
+
+
+
 # define flask app
 app = flask.Flask(__name__, template_folder='templates')
 
 
 # basic landing route
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    # make call for list of games
-    response = requests.get(
-        'https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/2020REG/{0}?key=d8b5ea01537141eb9a320f95994b7109'.format(
-            default_week))
+@app.route('/2020', methods=['GET', 'POST'])
+def twenty_twenty():
 
-    df = pd.DataFrame.from_dict(response.json())  # turn into dataframe
-    # df = df[['AwayTeam','HomeTeam']] # transform into only team names
+    # get the week 
+    response = requests.get('https://api.sportsdata.io/v3/nfl/scores/json/CurrentWeek?key=d8b5ea01537141eb9a320f95994b7109')
+    week = response.json()
+
+    # fetch data
+    sql_string = 'select games from games_data where year = 2020'
+    cur.execute(sql_string)
+    response = cur.fetchall()[1]
+
+
+
+
 
     # load page
     if flask.request.method == 'GET':
+        # load games
+        df = pd.read_json(response[0]).T
         return flask.render_template('main_page.html',
-                                     games=df)
+                                     games=df[df.Week == default_week],function="twenty_twenty", year=2020, week=week)
 
     if flask.request.method == 'POST':
+
+        df = pd.read_json(response[0]).T
         selected_week = flask.request.form['week_user_selected']
-        selected_year = flask.request.form['year_user_selected2']
-        year_api_dict = {'2017': 'a',
-                         '2018': 'b',
-                         '2019': 'c',
-                         '2020': 'https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/2020REG/{0}?key=d8b5ea01537141eb9a320f95994b7109'}
-        api_call = year_api_dict[selected_year]
 
-        response = requests.get(
-            api_call.format(
-                selected_week))
-        df = pd.DataFrame.from_dict(response.json())
         return flask.render_template('main_page.html',
-                                     games=df,
-                                     selected_week=selected_week,
-                                     selected_year=selected_year)
+                                     games=df[df.Week == int(selected_week)],
+                                     selected_week=selected_week,function="twenty_twenty", year=2020, week=week)
+
+# basic landing route
+@app.route('/2019', methods=['GET', 'POST'])
+def twenty_nineteen():
+
+    # fetch data
+    sql_string = 'select games from games_data where year = 2019'
+    cur.execute(sql_string)
+    response = cur.fetchall()[1]
 
 
+
+
+
+    # load page
+    if flask.request.method == 'GET':
+        # load games
+        df = pd.read_json(response[0]).T
+        return flask.render_template('main_page.html',
+                                     games=df[df.Week == default_week],function="twenty_nineteen",year=2019)
+
+    if flask.request.method == 'POST':
+
+        df = pd.read_json(response[0]).T
+        selected_week = flask.request.form['week_user_selected']
+
+        return flask.render_template('main_page.html',function="twenty_nineteen",
+                                     games=df[df.Week == int(selected_week)],
+                                     selected_week=selected_week, year=2019)
+
+# basic landing route
+@app.route('/2018', methods=['GET', 'POST'])
+def twenty_eighteen():
+
+    # fetch data
+    sql_string = 'select games from games_data where year = 2018'
+    cur.execute(sql_string)
+    response = cur.fetchall()[1]
+
+
+
+
+
+    # load page
+    if flask.request.method == 'GET':
+        # load games
+        df = pd.read_json(response[0]).T
+        return flask.render_template('main_page.html',function="twenty_eighteen",
+                                     games=df[df.Week == default_week], year=2018, week=week)
+
+    if flask.request.method == 'POST':
+
+        df = pd.read_json(response[0]).T
+        selected_week = flask.request.form['week_user_selected']
+
+        return flask.render_template('main_page.html',function="twenty_eighteen",
+                                     games=df[df.Week == int(selected_week)],
+                                     selected_week=selected_week, year=2018, week=week)
+
+# basic landing route
+@app.route('/2017', methods=['GET', 'POST'])
+def twenty_seventeen():
+
+    # fetch 2020 data
+    sql_string = 'select games from games_data where year = 2017'
+    cur.execute(sql_string)
+    response = cur.fetchall()[1]
+
+
+
+
+
+    # load page
+    if flask.request.method == 'GET':
+        # load games
+        df = pd.read_json(response[0]).T
+        return flask.render_template('main_page.html',function="twenty_seventeen",
+                                     games=df[df.Week == default_week+1], year=2017)
+
+    if flask.request.method == 'POST':
+
+        df = pd.read_json(response[0]).T
+        selected_week = flask.request.form['week_user_selected']
+
+        return flask.render_template('main_page.html',function="twenty_seventeen",
+                                     games=df[df.Week == int(selected_week)],
+                                     selected_week=selected_week, year=2017)
 if __name__ == '__main__':
     app.run(debug=True)
