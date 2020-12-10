f4db6632b958cbcbe5942ce7edd49c247b4f6c94
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-12-10 11:13:19 -0500

    turn  off debug mode

--- app.py
+++ app.py
@@ -344,4 +344,4 @@ def specific_matchups():
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True)
+    app.run()
